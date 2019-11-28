e0fca11844d0532fc94a1f51a66139dec9ac62d7
Anxo Rodriguez <anxolin@gmail.com>
2019-11-28 12:02:00 +0100

    Test fix testPathIgnorePatterns

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,16 +12,6 @@ module.exports = {
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
-    // FIXME: https://github.com/gnosis/dex-telegram/issues/14
-    // // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    // '@typescript-eslint/explicit-function-return-type': [
-    //   'error',
-    //   {
-    //     allowTypedFunctionExpressions: true,
-    //   },
-    // ],
-
     // Disabled the 'no-unused-vars' error (typescript/no-unused-vars is used instead)
     'no-unused-vars': 'off',
   },
