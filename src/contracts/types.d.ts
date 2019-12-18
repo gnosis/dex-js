60634523260c06056f25bf2a41b0d87be62c92fb
Anxo Rodriguez <anxolin@gmail.com>
2019-12-18 23:17:47 +0100

    Improve the exports

--- src/contracts/types.d.ts
+++ src/contracts/types.d.ts
@@ -1,17 +1,6 @@
 import { EventEmitter } from 'events'
 import { EventLog } from 'web3-core'
 import { EventOptions } from 'web3-eth-contract'
-
-export type Callback<T> = (error: Error, result: T) => void
-
-export interface ContractEventLog<T> extends EventLog {
-  returnValues: T
-}
-
-export interface ContractEventEmitter<T> extends EventEmitter {
-  on(event: 'connected', listener: (subscriptionId: string) => void): this
-  on(event: 'data' | 'changed', listener: (event: ContractEventLog<T>) => void): this
-  on(event: 'error', listener: (error: Error) => void): this
-}
+import { Callback, ContractEventLog, ContractEventEmitter } from './gen/types'
 
 export type ContractEvent<T> = (options?: EventOptions, cb?: Callback<ContractEventLog<T>>) => ContractEventEmitter<T>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { TokenDetailsConfig } from 'types'
+import * as genTypes from './contracts/gen/types'
 
 // Types & constants
 export * from './types'
@@ -12,7 +13,17 @@ export * from './utils'
 // TODO: For now the contract definitions are declared in dex-js. But they will be defined in dex-contracts
 export * from './contracts/Erc20Contract'
 export * from './contracts/BatchExchangeContract'
-export * from './contracts/gen/types'
+export { EventOptions } from 'web3-eth-contract'
+export { ContractEvent } from './contracts/types'
+export {
+  EstimateGasOptions,
+  Callback,
+  TransactionObject,
+  ContractEventLog,
+  ContractEventEmitter,
+  Tx,
+  BlockType
+} from './contracts/gen/types'
 
 // Json list
 export const tokenList: TokenDetailsConfig[] = require('tokenList.json')
