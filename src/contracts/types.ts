76f8a9396d48a4078c2c6fbcbcc7e767aa15963b
Anxo Rodriguez <anxolin@gmail.com>
2019-12-18 23:30:56 +0100

    Make sure the contract gets included in package

--- src/contracts/types.ts
+++ src/contracts/types.ts
@@ -1,5 +1,3 @@
-import { EventEmitter } from 'events'
-import { EventLog } from 'web3-core'
 import { EventOptions } from 'web3-eth-contract'
 import { Callback, ContractEventLog, ContractEventEmitter } from './gen/types'
 
