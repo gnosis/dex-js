fe386d339e400cf9d5f708446de95ac4837a3e24
Anxo Rodriguez <anxolin@gmail.com>
2019-12-18 14:20:14 +0100

    Fix error importing ABI

--- test/sandbox/contracts/printEventTopics.ts
+++ test/sandbox/contracts/printEventTopics.ts
@@ -11,7 +11,7 @@ require('dotenv').config()
 const log = new Logger('sandbox:contracts/printEventTopics')
 
 async function exec (): Promise<void> {
-  const abi = require('contracts/BatchExchange.json')
+  const abi = require('contracts/BatchExchangeAbi.json')
   const events = abi.filter((def: AbiItem) => def.type === 'event')
 
   log.info('Found %d events:', events.length)
