fd82aa1c0f944359a3f156a782e446741620b3dc
Anxo Rodriguez <anxolin@gmail.com>
2019-12-23 12:19:32 +0100

    Get ABI from dex-contracts

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -2,6 +2,8 @@ import assert from 'assert'
 import { web3 } from 'helpers/web3'
 import { BatchExchangeContract } from './BatchExchangeContract'
 import { Erc20Contract } from './Erc20Contract'
+import { abi as batchExchangeAbi } from '@gnosis.pm/dex-contracts/build/contracts/BatchExchange.json'
+import { AbiItem } from 'web3-utils'
 
 function getBatchExchange (): BatchExchangeContract {
   const { STABLE_COIN_CONTRACT_ADDRESS } = process.env
@@ -9,8 +11,7 @@ function getBatchExchange (): BatchExchangeContract {
   const stableCoinContractAddress = STABLE_COIN_CONTRACT_ADDRESS as string
 
   // FIXME: There's an issue with this conversion: https://github.com/gnosis/dex-telegram/issues/14
-  const abi = require('./abi/BatchExchange.json')
-  const unknownContract = new web3.eth.Contract(abi, stableCoinContractAddress) as unknown
+  const unknownContract = new web3.eth.Contract(batchExchangeAbi as AbiItem[], stableCoinContractAddress) as unknown
   return unknownContract as BatchExchangeContract
 }
 
