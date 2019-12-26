8837f8773b8e7540e39f20d8491b7643327c365d
Anxo Rodriguez <anxolin@gmail.com>
2019-12-26 17:45:06 +0100

    Rename constructor names

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -13,7 +13,7 @@ export * from './Erc20Contract'
 export const erc20Abi = erc20AbiJson as AbiItem[]
 export const batchExchangeAbi = batchExchangeAbiJson as AbiItem[]
 
-export function getBatchExchangeContract (web3: Web3, address?: string): BatchExchangeContract {
+export function createBatchExchangeContract (web3: Web3, address?: string): BatchExchangeContract {
   const batchExchangeAddress = address || process.env.STABLE_COIN_CONTRACT_ADDRESS
 
   assert(batchExchangeAddress, 'address param, or STABLE_COIN_CONTRACT_ADDRESS env is required')
@@ -25,7 +25,7 @@ export function getBatchExchangeContract (web3: Web3, address?: string): BatchEx
   return unknownContract as BatchExchangeContract
 }
 
-export function getErc20Contract (web3: Web3): Erc20Contract {
+export function createErc20Contract (web3: Web3): Erc20Contract {
   // FIXME: There's an issue with this conversion: https://github.com/gnosis/dex-telegram/issues/14
   const unknownContract = new web3.eth.Contract(erc20Abi as AbiItem[]) as any
   return unknownContract as Erc20Contract
