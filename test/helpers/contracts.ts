f009e8c74ed3adf8b09b278cc4f7f620fe690f7f
Anxo Rodriguez <anxolin@gmail.com>
2019-12-26 17:45:06 +0100

    Rename constructor names

--- test/helpers/contracts.ts
+++ test/helpers/contracts.ts
@@ -1,5 +1,5 @@
-import { getBatchExchangeContract, getErc20Contract } from 'contracts'
+import { createBatchExchangeContract, createErc20Contract } from 'contracts'
 import { web3 } from './web3'
 
-export const batchExchangeContract = getBatchExchangeContract(web3)
-export const erc20Contract = getErc20Contract(web3)
+export const batchExchangeContract = createBatchExchangeContract(web3)
+export const erc20Contract = createErc20Contract(web3)
