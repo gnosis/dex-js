34d2f6dcb3ad0406e479ef41df34df91b827f291
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-03-11 09:24:29 -0700

    Added calculatePrice utils function

---
+++ src/utils/price.ts
@@ -0,0 +1,39 @@
+import BigNumber from 'bignumber.js'
+import BN from 'bn.js'
+
+interface Token {
+  amount: BN | BigNumber | string
+  decimals?: number
+}
+
+interface CalculatePriceParams {
+  buyToken: Token
+  sellToken: Token
+}
+
+/**
+ * Calculates price from either BN, BigNumber or string amounts
+ * based on decimal precision of each token.
+ *
+ * Decimals defaults to 0.
+ * Use case is to calculate the price when values already in the same unit
+ * and no adjustment is required.
+ *
+ * Returns price in BigNumber and leave formatting up to the caller
+ */
+export function calculatePrice({
+  buyToken: { amount: buyAmount, decimals: buyDecimals = 0 },
+  sellToken: { amount: sellAmount, decimals: sellDecimals = 0 },
+}: CalculatePriceParams): BigNumber {
+  // convert to BigNumber
+  const numerator = new BigNumber(buyAmount.toString())
+  const denominator = new BigNumber(sellAmount.toString())
+
+  if (buyDecimals >= sellDecimals) {
+    const precisionFactor = 10 ** (buyDecimals - sellDecimals)
+    return numerator.dividedBy(denominator.multipliedBy(precisionFactor))
+  } else {
+    const precisionFactor = 10 ** (sellDecimals - buyDecimals)
+    return numerator.multipliedBy(precisionFactor).dividedBy(denominator)
+  }
+}
