167b9f3db7ac8c650e3db548c690e6af786c6ec3
Velenir <Velenir@users.noreply.github.com>
2020-08-05 13:04:24 +0300

    Reconnect WS provider (#166)
    * update web3
    
    * pass config to WebsocketProvider

--- src/helpers/web3.ts
+++ src/helpers/web3.ts
@@ -1,5 +1,6 @@
 import assert from 'assert'
 import Web3 from 'web3'
+import { HttpProvider, WebsocketProvider } from 'web3-core'
 import Logger from 'helpers/Logger'
 
 export function createWeb3 (url?: string): Web3 {
@@ -7,6 +8,19 @@ export function createWeb3 (url?: string): Web3 {
   const nodeUrl = url || process.env.NODE_URL
 
   log.info('Connecting to ethereum using web3: %s', nodeUrl)
-  assert(nodeUrl, 'url param, or NODE_URL env var is required')
-  return new Web3(nodeUrl as string)
+  assert(nodeUrl && /^(http|ws)s?:\/\/.+/.test(nodeUrl), 'url param, or NODE_URL env var must be a valid url')
+
+  const provider = /^wss?:\/\/.*/.test(nodeUrl)
+    ? new WebsocketProvider(nodeUrl, {
+      timeout: 30000, // ms
+      // Enable auto reconnection
+      reconnect: {
+        auto: true,
+        delay: 5000, // ms between connection attempts
+        onTimeout: true,
+      },
+    })
+    : new HttpProvider(nodeUrl)
+
+  return new Web3(provider)
 }
