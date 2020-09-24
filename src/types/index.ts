4198d9751a4890e9c8bf199ae54e9924957a26d4
Anxo Rodriguez <anxolin@gmail.com>
2020-09-24 11:49:53 +0200

    Add additional types for tokens

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,31 @@
 export type Command = () => void
 export type AsyncCommand = () => Promise<void>
 
-export interface TokenDetails {
-  id: number
-  name?: string
-  symbol?: string
-  decimals?: number
-  address: string
+export interface WithAddressMainnet {
   addressMainnet?: string
 }
 
-export interface TokenDetailsConfig extends Omit<TokenDetails, 'address'> {
+export interface WithId {
+  id: string
+}
+
+export interface WithSymbolAndName {
+  symbol?: string
+  name?: string
+}
+
+export interface WithAddress {
+  address: string
+}
+
+export interface WithDecimals {
+  decimals: number
+}
+
+export type TokenErc20 = WithSymbolAndName & WithAddress & WithDecimals
+export type Token = TokenErc20 & WithAddressMainnet & Partial<WithId>
+export type TokenDex = TokenErc20 & WithAddressMainnet & WithId
+
+export interface TokenDetailsConfig extends Omit<TokenDex, 'address'> {
   addressByNetwork: { [networkId: string]: string | undefined }
 }
